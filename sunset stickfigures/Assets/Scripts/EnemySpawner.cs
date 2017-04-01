a27bcf7092abef29b4806af9ed111b1ee1025488
greenjack3 <jacyna20@gmail.com>
2017-04-01 11:54:25 +0200

    v0.3

---
+++ sunset stickfigures/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemySpawner : MonoBehaviour {
+
+    public float Timer;
+    public float minT;
+    public float maxT;
+    public bool startTimer = false;
+    public GameObject[] enemy;
+
+
+	void Start ()
+    {
+        Timer = Random.Range(minT, maxT);
+        startTimer = true;
+		
+	}
+	
+	
+
+	void Update ()
+    {
+        if(startTimer == true)
+        {
+            Timer -= Time.deltaTime;
+        }
+		if(Timer <= 0)
+        {
+            GameObject enem = enemy[Random.Range(0, enemy.Length)];
+            GameObject instance = Instantiate(enem, gameObject.transform.position, gameObject.transform.rotation);
+            Timer = Random.Range(minT, maxT);
+            startTimer = false;
+        }
+	}
+
+
+    public void Spwaning()
+    {
+        Timer = Random.Range(minT, maxT);
+        startTimer = true;
+    }
+}
---
+++ sunset stickfigures/Assets/Scripts/Hittable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hittable : MonoBehaviour {
+
+    public GameObject parent;
+    //public GameObject deathParticles;
+
+    public bool isPlayer;
+
+    void Death()
+    {
+        //Instantiate(deathParticles, transform);
+        Destroy(parent);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("trigger entered");
+        BulletBehavior bullet = other.GetComponent<BulletBehavior>();
+        if (bullet != null)
+        {
+            if (bullet.playersBullet)
+            {
+                if (isPlayer)
+                    return;
+            }
+            if (!bullet.playersBullet)
+            {
+                if (!isPlayer)
+                    return;
+            }
+
+            Death();
+        }
+    }
+}
